a8dbc63d2cbcc7be1338a004b54ba8a3b890486e
Mina Nur Cebi <minanurcebi@yandex.com>
2024-05-28 17:11:09 +0300

    Added main function to run various scripts and store results in the RESULTS structure.

---
+++ mainFunction.m
@@ -0,0 +1,14 @@
+function RESULTS = mainFunction()
+    % Main function to run various scripts and store the results
+    % in the RESULTS structure.
+
+    % Add paths to the necessary function and script directories
+    addpath('Functions\')
+    addpath('Scripts\')
+
+    % Run the necessary scripts to perform calculations
+
+    % Convert CSV data to a structured format with timeseries
+    run('csv2struct.m')
+
+end
