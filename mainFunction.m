25e142d037998bbb4a67c82bf75f5828afeffed9
Mina Nur Cebi <minanurcebi@yandex.com>
2024-05-28 17:31:46 +0300

    Updated mainFunction to store results in the RESULTS structure.

--- mainFunction.m
+++ mainFunction.m
@@ -22,5 +22,33 @@ function RESULTS = mainFunction()
     
     % Calculate airspeed components (u, v, w)
     run('calculateAirSpeedComponents.m')
+    
+    % Store the results in the RESULTS structure
+    % Retrieve the time data from the dataset structure
+    Time = eval([char(dataset_struct_name) + "time_sn" + ".Data;"]);
+
+    % Store aerodynamic coefficients in the RESULTS structure
+    RESULTS.CL = timeseries(CL, Time);
+    RESULTS.CD = timeseries(CD, Time);
+    RESULTS.CM = timeseries(CM, Time);
+
+    % Store Euler angles in the RESULTS structure
+    RESULTS.YAW = timeseries(YAW, Time);
+    RESULTS.PITCH = timeseries(PITCH, Time);
+    RESULTS.ROLL = timeseries(ROLL, Time);
+
+    % Store NED coordinates in the RESULTS structure
+    RESULTS.LATITUDE = timeseries(LATITUDE, Time);
+    RESULTS.LONGITUDE = timeseries(LONGITUDE, Time);
+    RESULTS.ALTITUDE = timeseries(ALTITUDE, Time);
+
+    % Store airspeed components in the RESULTS structure
+    RESULTS.u = timeseries(u, Time);
+    RESULTS.v = timeseries(v, Time);
+    RESULTS.w = timeseries(w, Time);
+    
+    % Clear variables to free up memory
+    clearvars scriptname scriptname2 scriptname3 scriptname4 scriptname5 CL CD CM ...
+    YAW PITCH ROLL LATITUDE LONGITUDE ALTITUDE u v w Time dataset_struct_name
 
 end
