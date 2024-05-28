86a008afe983ccffd33cc4a9f0f9afa303010e9d
Mina Nur Cebi <minanurcebi@yandex.com>
2024-05-28 17:36:23 +0300

    Added processTelemetryFile function to read and process the selected telemetry CSV file

--- telemetriGUI.m
+++ telemetriGUI.m
@@ -8,4 +8,31 @@ function telemetriGUI
     axesPanel = uipanel(fig, 'Position', [240, 20, 540, 560]);
     plotButton = uibutton(fig, 'push', 'Text', 'Plot Graph', 'Position', [130, 550, 100, 30], 'ButtonPushedFcn', @(src, event) plotButtonPushed());
 
+    % Storing the telemetry data globally
+    global telemetryData;
+
+    % Function to be executed when a file is selected
+    function fileButtonPushed()
+        [fileName, pathName] = uigetfile({'*.csv', 'CSV Files (*.csv)'}, 'Select Telemetry File');
+        if fileName == 0
+            uialert(fig, 'No file selected.', 'File Selection Error');
+            return;
+        end
+        filePath = fullfile(pathName, fileName);
+        telemetryData = processTelemetryFile(filePath);
+        variableNames = fieldnames(telemetryData);
+        disp(telemetryData);
+        variableList.Items = variableNames;
+    end
+
+    % Function to be executed when the 'Plot Graph' button is pressed
+    function plotButtonPushed()
+        updatePlots(variableList.Value);
+    end
+    % Function to process the selected telemetry file
+    function data = processTelemetryFile(filePath)
+        dataTable = readtable(filePath);
+        data = table2struct(dataTable);
+    end
+
 end
